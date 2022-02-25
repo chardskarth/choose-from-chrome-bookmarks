98919b9e1e6fd17e5fd5406f80a51960ef1943f8
Richard Domingo <chardskarth@gmail.com>
2022-02-26 02:43:30 +0800

    fixup! initial commit

--- README.md
+++ README.md
@@ -2,30 +2,47 @@
 
 
 ## What does this do?
+TODO: Create gif screen recording
 
 
 ## Dependencies
 
 The following tools are required to be installes
-1. [jq]()
-1. [choose-gui]()
+1. [kscript](https://github.com/holgerbrandl/kscript)
+1. [choose-gui](https://github.com/chipsenkbeil/choose)
+1. [skhd](https://www.google.com/search?q=skhd&oq=skhd&aqs=chrome..69i57j35i39l2j0i20i263i512j0i512l3j69i60.1471j0j9&sourceid=chrome&ie=UTF-8). This is optional. You can use a different tool to trigger the script
 
 
 ## Installation
 
+```
+# First, install the dependencies. (kscript, zsh and choose-gui)
+git clone git@github.com:chardskarth/choose-from-chrome-bookmarks.git
+# add `bin` directory in exported path
+echo "PATH=\$PATH:<location of this repo>/bin" >> ~/.profile
+```
+
+
+### Then adding a hotkey for it... I use skhd for this.
+```
+brew install skhd
+echo "ctrl - return : choose-from-chrome-bookmarks triggerchoose &>> ~/.logs/choose_from_bookmarks.log" >> ~/.config/skhd/skhdrc
+```
+
+## Uninstalling
+You can remove this repository from $PATH
+
 
 ## Limitation
 
-**I'm not yet a pro in creating zsh plugins.**
-
-This plugin is coded very minimally, and its not yet battle tested. 
+This coded very minimally, and its not yet battle tested. 
 Right now it just works for my use case, but feel free to submit an MR.
 
 
-Some gotchas to help you get started in contributing:
-1. You can see log files in [~/.logs/]()
-2. There's no configuration yet, you'll have to manually [change them here]()
+Some gotchas:
+1. [Install script]() is very simple, it just creates a symlink to `~/.localscripts`
+2. Chrome bookmarks path is hard [here]()
+1. Logging is can be found at [~/.logs/]()
+2. There's no configuration yet, you'll have to manually [configure paths here]()
 
-
-Cheers
 
---
+++ lib/read-bookmarks-from-directory.kts
@@ -0,0 +1,75 @@
+
+#!/bin/zsh
+
+bookmarkfile="$1"
+
+kscript - "$bookmarkfile"<<__EOF_END
+@file:DependsOn("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.30")
+@file:DependsOn("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
+
+@file:CompilerOpts("-Xplugin=$HOME/.sdkman/candidates/kotlin/current/lib/kotlinx-serialization-compiler-plugin.jar")
+
+import kotlinx.serialization.*
+import java.io.File
+import kotlinx.serialization.json.*
+
+val chromeBookmarksFilePath = args[0]
+
+val chromeFileContents = File(chromeBookmarksFilePath)
+    .inputStream()
+    .readBytes()
+    .toString(Charsets.UTF_8)
+
+val browser = Json.decodeFromString<JsonObject>(chromeFileContents)
+val bookmarkContents = browser["roots"]?.jsonObject?.get("bookmark_bar")?.jsonObject?.get("children").toString()
+val json = Json { ignoreUnknownKeys = true
+}
+val bookmarkBarItems: List<BookmarkBarItem> = json.decodeFromString(bookmarkContents)
+
+bookmarkBarItems.flatMap {
+    flattenByFolder(it.name, it)
+}.forEach {
+    println("\${it.name} >> \${it.url}")
+}
+
+//println(json.encodeToString(bookmarkBarItems))
+//println(json.encodeToString(flattenedByFolder))
+
+fun flattenByFolder(key: String, bookmarkBarItem: BookmarkBarItem): List<FlattenedByFolder> {
+    return bookmarkBarItem.getBookmarks("\${key.smartTrailing(".")}\${bookmarkBarItem.name}") + bookmarkBarItem.getFolders().flatMap {
+        flattenByFolder("\$key.\${it.name}", it)
+    }
+}
+
+fun String.smartTrailing(toAppend: String) = if (this != "") "\$this\$toAppend" else this
+
+@Serializable
+data class BookmarkBarItem(
+    val children: List<BookmarkBarItem>? = null,
+    val url: String? = null,
+    val name: String,
+    val type: String,
+
+) {
+    private val isFolder = type == "folder"
+    fun getBookmarks(key: String) = children?.filter {
+        it.isFolder.not()
+    }?.map {
+      FlattenedByFolder("\$key.\${it.name}", it.url!!)
+    } ?: listOf()
+
+    fun getFolders() = children?.filter {
+        it.isFolder
+    } ?: listOf()
+
+}
+
+@Serializable
+data class FlattenedByFolder(
+    val name: String,
+    val url: String,
+)
+__EOF_END
+
+printf '%s' "$res"
+
